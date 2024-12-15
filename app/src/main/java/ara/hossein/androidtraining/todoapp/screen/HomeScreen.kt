32e067021a8ea64a8fc1086e0ec40da60722418a
Hossein Hashemi <91213707+hashemi-hossein@users.noreply.github.com>
2024-12-15 22:09:36 +0100

    Utilize database in HomeScreen

--- app/src/main/java/ara/hossein/androidtraining/todoapp/screen/HomeScreen.kt
+++ app/src/main/java/ara/hossein/androidtraining/todoapp/screen/HomeScreen.kt
@@ -12,18 +12,34 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ara.hossein.androidtraining.todoapp.data.TodoDatabase
+import ara.hossein.androidtraining.todoapp.data.TodoEntity
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+
     val text = remember { mutableStateOf("") }
-    val items = remember { mutableStateListOf<String>("Apple","Egg") }
+    val items = remember { mutableStateListOf<TodoEntity>() }
+
+    LaunchedEffect(true) {
+        TodoDatabase.getDatabase(context).todoDao().getAll().collect { todoItems->
+            items.clear()
+            items.addAll(todoItems)
+        }
+    }
 
     Column(
         modifier = Modifier.padding(16.dp),
@@ -35,7 +51,11 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         ) {
             OutlinedTextField(value = text.value, onValueChange = { text.value = it })
             Button(onClick = {
-
+                scope.launch {
+                    TodoDatabase.getDatabase(context).todoDao()
+                        .insert(TodoEntity(id = 0,text = text.value, isDone = false))
+                    text.value = ""
+                }
             }) { Text("Add") }
         }
         LazyColumn(modifier = Modifier.padding(top = 16.dp)) {
@@ -45,7 +65,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                         .fillMaxWidth()
                         .padding(8.dp)
                 ) {
-                    Text(it, modifier = Modifier.padding(16.dp))
+                    Text(it.text, modifier = Modifier.padding(16.dp))
                 }
             }
         }
