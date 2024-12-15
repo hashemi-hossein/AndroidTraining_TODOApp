2ed6fa7fcc495300c779ef7fc7d1725cbad570d5
Hossein Hashemi <91213707+hashemi-hossein@users.noreply.github.com>
2024-12-15 19:54:20 +0100

    Initial commit

---
+++ app/src/main/java/ara/hossein/androidtraining/todoapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package ara.hossein.androidtraining.todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ara.hossein.androidtraining.todoapp.ui.theme.AndroidTraining_TODOAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidTraining_TODOAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidTraining_TODOAppTheme {
+        Greeting("Android")
+    }
+}
