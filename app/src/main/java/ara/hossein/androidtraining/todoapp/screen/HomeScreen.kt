25764b31fac8c25aca5dc5138c4270fae2eb025c
Hossein Hashemi <91213707+hashemi-hossein@users.noreply.github.com>
2024-12-15 21:05:41 +0100

    Implement UI

--- app/src/main/java/ara/hossein/androidtraining/todoapp/screen/HomeScreen.kt
+++ app/src/main/java/ara/hossein/androidtraining/todoapp/screen/HomeScreen.kt
@@ -1,15 +1,58 @@
 package ara.hossein.androidtraining.todoapp.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    val text = remember { mutableStateOf("") }
+    val items = remember { mutableStateListOf<String>("Apple","Egg") }
 
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            OutlinedTextField(value = text.value, onValueChange = { text.value = it })
+            Button(onClick = {
+
+            }) { Text("Add") }
+        }
+        LazyColumn(modifier = Modifier.padding(top = 16.dp)) {
+            items(items) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Text(it, modifier = Modifier.padding(16.dp))
+                }
+            }
+        }
+    }
 }
 
-@Preview
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun HomeScreenPreview() {
     HomeScreen()
